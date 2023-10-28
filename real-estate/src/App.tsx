35ab9c056a9348dbb903ec51613de534d2b73e77
masud1901 <akmolmasud5@gmail.com>
2023-10-29 01:51:50 +0600

    made project with vite and added talwind

---
+++ real-estate/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p className='text-4xl bg-slate-300'>Hello World </p>
+    </>
+  )
+}
+
+export default App
