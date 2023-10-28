4233d03eef44414d927e5305224d93f6a44fc91e
masud1901 <akmolmasud5@gmail.com>
2023-10-29 05:30:32 +0600

    added the routes

--- real-estate/src/App.tsx
+++ real-estate/src/App.tsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router,Routes,Route  } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import ForgotPassword from './pages/ForgotPassword'
+import Offers from './pages/Offers'
+
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <p className='text-4xl bg-slate-300'>Hello World </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/sign-in' element={<Signin/>} />
+        <Route path='/sign-up' element={<Signup/>} />
+        <Route path='/forgot-password' element={<ForgotPassword/>} />
+        <Route path='/offers' element={<Offers/>} />
+      </Routes>
+    </Router>
+  </>
   )
 }
+
 
 export default App
