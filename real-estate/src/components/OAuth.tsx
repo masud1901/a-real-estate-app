0ac74091c7a200e423e320b526a145dddcee5d70
masud1901 <akmolmasud5@gmail.com>
2023-10-31 22:33:42 +0600

    Completed the google sign up part

--- real-estate/src/components/OAuth.tsx
+++ real-estate/src/components/OAuth.tsx
@@ -1,10 +1,52 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { app, db } from "../firebase";
+import { useNavigate } from "react-router";
+import {
+  Timestamp,
+  doc,
+  getDoc,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
 
 export default function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth(app);
+      const proider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, proider);
+      const { user } = result;
+
+      // Check if the user is already a member
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with google");
+    }
+  }
+
   return (
-    <button className="flex items-center justify-center my-4 w-full bg-red-500 hover:bg-red-600 text-white font-semibold uppercase py-3 rounded-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none active:bg-red-700">
-      <FcGoogle className="text-2xl bg-white rounded-full mr-2" /> Contunue with Google
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="flex items-center justify-center my-4 w-full bg-red-500 hover:bg-red-600 text-white font-semibold uppercase py-3 rounded-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none active:bg-red-700"
+    >
+      <FcGoogle className="text-2xl bg-white rounded-full mr-2" /> Contunue with
+      Google
     </button>
   );
 }
